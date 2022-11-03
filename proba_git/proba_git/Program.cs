596b295288c25a89032ffac9c9ed5f78241d6413
Ana Boskovic <anoka1810@gmal.com>
2022-11-03 13:32:48 +0100

    Add new loop

--- proba_git/proba_git/Program.cs
+++ proba_git/proba_git/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+for(int i = 0; i < 10; i++)
+{
+    if (i<5)
+    {
+        Console.WriteLine(i);
+    }
+}
