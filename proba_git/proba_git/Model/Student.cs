ea7dab68b7677c29a32cd9d191a9bdebe9396f07
Ana Boskovic <anoka1810@gmal.com>
2022-11-03 21:03:38 +0100

    implemented student class

---
+++ proba_git/proba_git/Model/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proba_git.Model
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int JMBG { get; set; }
+
+
+        public Student()
+        {
+
+        }
+
+
+    }
+}
